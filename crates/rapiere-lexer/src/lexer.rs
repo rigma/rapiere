50936b90cbc84ab2036ff93c843b3a7de4b6c9e4
rigma <rigbuntu@gmail.com>
2024-12-29 14:03:04 +0100

    feat(lexer): add `Lexer` struct

---
+++ crates/rapiere-lexer/src/lexer.rs
@@ -0,0 +1,81 @@
+use crate::{
+    errors::Error,
+    scanner::Scanner,
+    token::{Token, TokenKind},
+};
+use std::fmt;
+
+#[derive(Debug)]
+pub struct Lexer<'i> {
+    eof: bool,
+    input: &'i [u8],
+    scanner: Scanner,
+}
+
+impl<'i> Lexer<'i> {
+    #[inline(always)]
+    pub fn new(input: &'i [u8]) -> Self {
+        Self {
+            eof: false,
+            input,
+            scanner: Scanner::new(),
+        }
+    }
+
+    #[inline(always)]
+    pub fn column(&self) -> usize {
+        self.scanner.column()
+    }
+
+    #[inline(always)]
+    pub fn line(&self) -> usize {
+        self.scanner.line()
+    }
+
+    pub fn next_token(&mut self) -> Result<Option<Token>, Error> {
+        if self.eof {
+            return Ok(None);
+        }
+
+        let token = if let Some(token) = self.scanner.scan(self.input)? {
+            token
+        } else {
+            self.eof = true;
+
+            Token::new(
+                TokenKind::EOF,
+                self.scanner.line(),
+                self.scanner.column(),
+                0,
+            )
+        };
+
+        Ok(Some(token))
+    }
+
+    #[inline(always)]
+    pub fn reset(&mut self, input: &'i [u8]) {
+        self.eof = false;
+        self.input = input;
+        self.scanner.reset();
+    }
+}
+
+impl<'i> From<&'i [u8]> for Lexer<'i> {
+    #[inline(always)]
+    fn from(value: &'i [u8]) -> Self {
+        Self::new(value)
+    }
+}
+
+impl fmt::Display for Lexer<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Lexer(line = {}, column = {}, eof = {})",
+            self.scanner.line(),
+            self.scanner.column(),
+            self.eof
+        )
+    }
+}
--- crates/rapiere-lexer/src/lib.rs
+++ crates/rapiere-lexer/src/lib.rs
@@ -1,8 +1,10 @@
 mod errors;
+mod lexer;
 mod scanner;
 mod token;
 mod tokenizer;
 
 pub use errors::{Error, TokenizerError};
+pub use lexer::Lexer;
 pub use scanner::Scanner;
 pub use token::{Token, TokenKind, TokenValue};
